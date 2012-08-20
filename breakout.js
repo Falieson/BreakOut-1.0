e032e777b632054ab5ff20b5ead5334e5384578a
florianmettetal <florian.mettetal@gmail.com>
2012-08-19 21:30:09 -0700

    Got the slapper working!

--- breakout.js
+++ breakout.js
@@ -5,7 +5,7 @@
 // Created On: 2012 August, 18
 // Last Modified: 2012 August, 19 by FlorianMettetal
 //
-// Game can be found at: ./index.html
+// Game can be found at: http://FlorianMettetal.com/BreakOut-1.0/index.html
 //
 // Now we are going to make a Break Out game!
 // Log of my edits start here:
@@ -14,7 +14,7 @@
 // 3. Create start/restart button, changes score area as well
 // 4. replaced timer = setTimeout() with more efficient/modern requestAnimFrame
 // 5. Turn the paddle into a slapper
-
+// * Move everything from DOM to Canvas
 // * Create highscore board, "nickname" saved remotely, leaderboard
 // * Create destroyable blocks
 //////////////////////////////////////////////////////////////////////////////////
@@ -23,7 +23,7 @@
 //
 // Right now I'm working on the slapper function
 // 1. I broke the currentScore var, it no longer is being reset to 0
-// 3A. once that's fixed create var slapV = 6
+// 3A. once that's fixed create var slapV = 1
 // 3B. create if (slap && collision) ballDx = ballDy += slapV
 //
 //**  Make sure to update this! Last Updated: 20120819 01:06PST  **\\
@@ -48,17 +48,22 @@ var ball;
 var paddle;
 var score;
 //initial speeds
-var ballDx = 1;
-var ballDy = 1;
+var ballDx = 3;
+var ballDy = 3;
 var currentScore = 0;
+
+// Wall Config
+var wallDx = 0.25;
+var wallDy = 0.25;
 
 // Ball Position and Dimensions
 var ballY = 4;
 var ballX = 200;
 //var ballRad = 15px;
+var ballM = 1;
+var ballP = 1;
 
 //Paddle Position and Dimensions
-
 var paddleY = 470;
 var paddleX = 228;
 var paddleW = 64;
@@ -66,7 +71,10 @@ var paddleH = 16;
 
 //Slapper Config
 var slapY = 10;
-
+var slapDx = 1;
+var slapDy = 1;
+var slappable = false;
+ 
 // Board Dimensions
 var boardW = 500;
 //var boardH = 500;
@@ -89,11 +97,15 @@ function mouseListener(event){
 function reset(){
     ballX = 200;
     ballY = 4;
-    ballDx = 6;
-    ballDy = 6;
+    ballDx = 3;
+    ballDy = 3;
+    ballM = 1;
+
+    currentScore = 0;
+    
     paddleX = 228;
     paddleW = 64;
-    currentScore = 0;
+
     if( $('#tko').is('*') ){
         $('#tko').remove();
     }
@@ -127,6 +139,7 @@ function start(){
     render();
     $('#goButton').removeClass('goActive');
     $('#goButton').addClass('goInactive');
+    
     //end conditions
     if(ballY <= (paddleY) ){
         //still in play - keep the loop going
@@ -141,10 +154,25 @@ function detectCollisions(){
     //just reflect the ball on a collision
     //a more robust engine could change trajectory of ball based
     //on where the ball hits the paddle
-    if(collisionX())
+    if(collisionX()){
+        updateScore(1);
         ballDx *= -1;
-    if(collisionY())
+        /*if (ballDx>0){
+			ballDx -= wallDx;
+		}else{
+			ballDx += wallDx;
+		}*/	
+    }
+    if(collisionY()){
         ballDy *= -1;
+        updateScore(2);
+        /*if (ballDy>0){
+			ballDy -= wallDy;
+		}else{
+			ballDy += wallDy;
+		}*/
+    }
+
 }
 
 function collisionX(){
@@ -160,8 +188,28 @@ function collisionY(){
     //check to see if ball collided with paddle
     if(ballY > (paddleY-paddleH)){
         if(ballX > paddleX && ballX < paddleX + 64){
-            updateScore();
-            console.log(paddleY);
+        	if(slappable){
+        		if (ballDx>0){
+        			ballDx += slapDx;
+        		}else{
+        			ballDx -= slapDx;
+        		}
+        		if (ballDy>0){
+        			ballDy += slapDy;
+        		}else{
+        			ballDy -= slapDy;
+        		}
+        		
+        		/* output 
+        		console.log("Slappable: ",slappable);
+        		console.log("Power: ",ballP);
+        		console.log("Dx: ",ballDx);
+        		console.log("Dy: ",ballDy);
+        		console.log("-------------"); 
+        		*/
+        	}
+            updateScore(5);
+            //console.log("Paddle Height: ",paddleY);
             return true;
         }
     }
@@ -185,9 +233,12 @@ function slap(){
     $paddle.css({top: paddleY, width: paddleW});
     $paddle.css("background-color","lightblue");
 
+	slappable = true;
+
     setTimeout(function(){
         paddleY += slapY;
         createPaddle();
+		slappable = false;
     }, 200);
 }
 
@@ -196,10 +247,12 @@ function moveBall(){
     ballY += ballDy;
     ball.style.left = ballX;
     ball.style.top = ballY;
+    
+    var ballP = ballM * ballDx;
 }
 
-function updateScore(){
-    currentScore += 5;
+function updateScore(x){
+    currentScore += x;
     score.innerHTML = 'Score: ' + currentScore;
 }
 
